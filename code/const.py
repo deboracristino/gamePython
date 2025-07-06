da375d50445873d710bd23c5f077f063ce6293f0
debora.cristino <deboracristino@gmail.com>
2025-07-06 16:33:26 -0300

    Revert "creating menu interactions and background implementation"
    This reverts commit 338aee0515e5ae97f50f312c108f967e5a54360d.

---
+++ code/const.py
@@ -0,0 +1,14 @@
+# C
+COLOR_BLUE = (3, 252, 244)
+COLOR_WHITE = (255, 255, 255)
+COLOR_PURPLE = (175, 71, 210)
+
+#M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COOPERATIVE',
+               'SCOORE',
+               'EXIT')
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
